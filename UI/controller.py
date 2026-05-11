fcbad12a85b3b439ec12515a737dccd56c8307fc
EmanueleFerdinandoRicci <emanueleferdinando.ricci@gmail.com>
2026-05-11 10:51:17 +0200

    Lezione 11/05

--- UI/controller.py
+++ UI/controller.py
@@ -8,6 +8,32 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
+    def handleCerca(self,e):
+        source = self._model.getNodeFromId(int(self._view._txtIdOggetto.value))
+        lun = self._view._ddLun.value
+
+        if lun is None:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Attenzione ! Selezionare un valore di lunghezza tra le scelte", color="red"))
+            self._view.update_page()
+            return
+
+        lunInt = int(lun)
+
+        path,cost = self._model.getOptPath(source,lunInt)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Ho trovato un cammino che parte da {source} e costa {cost} "))
+        self._view.txt_result.controls.append(
+            ft.Text(f"Di seguito i nodi che compongono il cammino:"))
+
+        for p in path:
+            self._view.txt_result.controls.append(
+                ft.Text(p))
+
+        self._view.update_page()
+
+
     def handleAnalizzaOggetti(self, e):
         self._model.buildGraph()
         self._view.txt_result.controls.clear()
@@ -47,4 +73,21 @@ class Controller:
         self._view.txt_result.controls.clear()
         self._view.txt_result.controls.append(
             ft.Text(f"La componente connessa contenente l'oggetto con id {idOggetto} è composta di {sizeCompConn} nodi", color="green"))
+        self._view.update_page()
+
+        # Sblocco dei componenti (assicurati che i nomi corrispondano a View)
+        self._view._ddLun.disabled = False
+        self._view._btnCerca.disabled = False
+
+        # Creazione della lista di opzioni
+        lunValues = list(range(2, sizeCompConn))
+
+        # PULIZIA: Rimuovi le opzioni precedenti
+        self._view._ddLun.options.clear()
+
+        # POPOLAMENTO: Usa un ciclo for o list comprehension (non solo map)
+        for lun in lunValues:
+            self._view._ddLun.options.append(ft.dropdown.Option(str(lun)))
+
+        # REFRESH: Fondamentale per vedere i cambiamenti nel Dropdown
         self._view.update_page()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -10,6 +12,42 @@ class Model:
         self._idMapAO = {}
         for n in self._nodes:
             self._idMapAO[n.object_id] = n
+        self._optPath = []
+        self._optCost = 0
+
+    def getOptPath(self,source,lun):
+        self._optPath = []
+        self._optCost = 0
+        parziale = [source]
+
+        for n in self._graph.neighbors(source):
+            if n.classification == parziale[-1].classification:
+                parziale.append(n)
+                self._ricorsione(parziale,lun)
+                parziale.pop()
+        return self._optPath,self._optCost
+
+    def _ricorsione(self, parziale, lun):
+        if len(parziale) == lun:
+            #condizione di terminazione, allora parziale è lunga come lun
+            #per cui verifico che questa parziale sia meglio del mio best
+            #(condizione di ottimalità), ed in ogni caso esco
+            if self._costoPath(parziale) > self._optCost:
+                self._optCost = self._costoPath(parziale)
+                self._optPath = copy.deepcopy(parziale)
+            return
+        #se arrivo qui posso aggiungere ancora nodi
+        for n in self._graph.neighbors(parziale[-1]):
+            if n.classification == parziale[-1].classification:
+                parziale.append(n)
+                self._ricorsione(parziale, lun)
+                parziale.pop()
+
+    def _costoPath(self, path):
+        costo = 0
+        for i in range(0,len(path)-1):
+            costo += self._graph[path[i]][path[i+1]]["weight"]
+        return costo
 
     def buildGraph(self):
         #aggiunge nodi
@@ -54,3 +92,6 @@ class Model:
 
     def hasNode(self,id_oggetto):
         return id_oggetto in self._idMapAO
+
+    def getNodeFromId(self,id_oggetto):
+        return self._idMapAO[id_oggetto]
